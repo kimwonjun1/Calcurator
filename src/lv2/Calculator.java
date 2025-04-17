eb7517ddfda538539b135b5937f68ba277b2b430
kimwonjun1 <monkeydmw@naver.com>
2025-04-17 13:29:00 +0900

    숫자 입력 기능 추가

--- src/lv2/Calculator.java
+++ src/lv2/Calculator.java
@@ -1,2 +1,21 @@
-package lv2;public class Calculator {
+package lv2;
+
+public class Calculator {
+    private int value1;
+    private int value2;
+    private  char op;
+    private int result;
+
+    public Calculator(int value1, int value2, char op) {
+        this.value1 = value1;
+        this.value2 = value2;
+        this.op = op;
+    }
+
+    public int calculate(int value1, int value2, char op) {
+        switch (op) {
+            case '+' : result = value1 + value2;
+        }
+        return result;
+    }
 }
