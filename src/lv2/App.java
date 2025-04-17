c1328df32709b71a6af361033ae36f44ed500d84
kimwonjun1 <monkeydmw@naver.com>
2025-04-17 14:35:16 +0900

    calculate() ã완료

--- src/lv2/App.java
+++ src/lv2/App.java
@@ -7,46 +7,57 @@ public class App {
         Scanner sc = new Scanner(System.in);
         int value1 = 0;
         int value2 = 0;
+        while (true) {
+            while (true) {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                // Scanner를 사용하여 문자열을 입력받음
+                String input1 = sc.nextLine();
+                try {
+                    value1 = Integer.parseInt(input1); // 입력받은 문자열을 int형으로 형변환
 
-        while(true) {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            // Scanner를 사용하여 문자열을 입력받음
-            String input1 = sc.nextLine();
-            try {
-                value1 = Integer.parseInt(input1); // 입력받은 문자열을 int형으로 형변환
-
-                if (value1 < 0) { // 양의 정수(0포함) 조건 확인
-                    System.out.println("양의 정수(0 포함)를 입력해주세요.");
-                    continue;
+                    if (value1 < 0) { // 양의 정수(0포함) 조건 확인
+                        System.out.println("양의 정수(0 포함)를 입력해주세요.");
+                        continue;
+                    }
+                    break;
+                } // try문에서 에러 발생시 실행
+                catch (NumberFormatException e) { // 실수나 문자를 입력한 경우 Integer.parseInt(input1)에서 에러 발생
+                    System.out.println("정수를 입력해주세요."); // 에러 처리
                 }
-                break;
-            } // try문에서 에러 발생시 실행
-            catch (NumberFormatException e) { // 실수나 문자를 입력한 경우 Integer.parseInt(input1)에서 에러 발생
-                System.out.println("정수를 입력해주세요."); // 에러 처리
             }
-        }
 
-        while(true) {
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            // Scanner를 사용하여 문자열을 입력받음
-            String input2 = sc.nextLine();
-            try {
-                value2 = Integer.parseInt(input2); // 입력받은 문자열을 int형으로 형변환
+            while (true) {
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                // Scanner를 사용하여 문자열을 입력받음
+                String input2 = sc.nextLine();
+                try {
+                    value2 = Integer.parseInt(input2); // 입력받은 문자열을 int형으로 형변환
 
-                if (value2 < 0) { // 양의 정수(0포함) 조건 확인
-                    System.out.println("양의 정수(0 포함)를 입력해주세요.");
-                    continue;
+                    if (value2 < 0) { // 양의 정수(0포함) 조건 확인
+                        System.out.println("양의 정수(0 포함)를 입력해주세요.");
+                        continue;
+                    }
+                    break;
+                } // try문에서 에러 발생시 실행
+                catch (NumberFormatException e) { // 실수나 문자를 입력한 경우 Integer.parseInt(input1)에서 에러 발생
+                    System.out.println("정수를 입력해주세요."); // 에러 처리
                 }
-                break;
-            } // try문에서 에러 발생시 실행
-            catch (NumberFormatException e) { // 실수나 문자를 입력한 경우 Integer.parseInt(input1)에서 에러 발생
-                System.out.println("정수를 입력해주세요."); // 에러 처리
             }
-        }
-        char op = sc.nextLine().charAt(0);
-        Calculator calc = new Calculator(value1, value2, op);
 
-        int result = calc.calculate(value1, value2, op);
-        System.out.println("연산 결과 : " + result);
+            System.out.print("사칙연산 기호를 입력하세요: ");
+
+            char op = sc.nextLine().charAt(0);
+            int result = 0;
+
+            Calculator calc = new Calculator(value1, value2, op);
+
+            result = calc.calculate();
+            System.out.println("연산 결과 : " + result);
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            /* exit을 입력 받으면 반복 종료 */
+            String exit = sc.nextLine();
+            if(exit.equals("exit")) break;
+        }
     }
 }
--- src/lv2/Calculator.java
+++ src/lv2/Calculator.java
@@ -1,9 +1,9 @@
 package lv2;
 
 public class Calculator {
-    private int value1;
-    private int value2;
-    private  char op;
+    private final int value1;
+    private final int value2;
+    private final char op;
     private int result;
 
     public Calculator(int value1, int value2, char op) {
@@ -12,10 +12,27 @@ public class Calculator {
         this.op = op;
     }
 
-    public int calculate(int value1, int value2, char op) {
+    public int calculate() {
         switch (op) {
-            case '+' : result = value1 + value2;
+            case '+':
+                result = value1 + value2;
+                break;
+            case '-':
+                result = value1 - value2;
+                break;
+            case '*':
+                result = value1 * value2;
+                break;
+            case '/':
+                result = value1 / value2;
+                break;
+            default:
+                System.out.println("연산자를 잘못 입력하였습니다.\n +, -, *, / 중에 입력해주세요.");
         }
         return result;
+    }
+
+    public void removeResult() {
+
     }
 }
