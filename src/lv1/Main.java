148183c1adf05c0f33778f92ec32bef39beb9f64
kimwonjun1 <monkeydmw@naver.com>
2025-04-17 11:58:35 +0900

    실ì숫자 입력창에 실수, 문자 입력에 ë대한 처리 완료

--- src/lv1/Main.java
+++ src/lv1/Main.java
@@ -12,26 +12,38 @@ public class Main {
 
             while(true) {
                 System.out.print("첫 번째 숫자를 입력하세요: ");
-                // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-                value1 = sc.nextInt();
+                // Scanner를 사용하여 문자열을 입력받음
+                String input1 = sc.nextLine();
+                try {
+                    value1 = Integer.parseInt(input1); // 입력받은 문자열을 int형으로 형변환
 
-                if (value1 < 0) { // 양의 정수(0포함) 조건 확인
-                    System.out.println("양의 정수(0 포함)를 입력해주세요.");
-                    continue;
+                    if (value1 < 0) { // 양의 정수(0포함) 조건 확인
+                        System.out.println("양의 정수(0 포함)를 입력해주세요.");
+                        continue;
+                    }
+                    break;
+                } // try문에서 에러 발생시 실행
+                catch (NumberFormatException e) { // 실수나 문자를 입력한 경우 Integer.parseInt(input1)에서 에러 발생
+                    System.out.println("정수를 입력해주세요."); // 에러 처리
                 }
-                break;
             }
 
             while(true) {
                 System.out.print("두 번째 숫자를 입력하세요: ");
-                // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-                value2 = sc.nextInt();
+                // Scanner를 사용하여 문자열을 입력받음
+                String input2 = sc.nextLine();
+                try {
+                    value2 = Integer.parseInt(input2); // 입력받은 문자열을 int형으로 형변환
 
-                if (value2 < 0) { // 양의 정수(0포함) 조건 확인
-                    System.out.println("양의 정수(0 포함)를 입력해주세요.");
-                    continue;
+                    if (value2 < 0) { // 양의 정수(0포함) 조건 확인
+                        System.out.println("양의 정수(0 포함)를 입력해주세요.");
+                        continue;
+                    }
+                    break;
+                } // try문에서 에러 발생시 실행
+                catch (NumberFormatException e) { // 실수나 문자를 입력한 경우 Integer.parseInt(input1)에서 에러 발생
+                    System.out.println("정수를 입력해주세요."); // 에러 처리
                 }
-                break;
             }
 
             sc.nextLine(); // ← 버퍼에 남은 줄바꿈 제거
